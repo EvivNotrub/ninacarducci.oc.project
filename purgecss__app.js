9720b57e1db35fcf127e18bfcf8de2fc165d54e8
EvivNotrub <barthwerle@yahoo.de>
2023-09-16 13:54:56 +0200

    config purgecss & run works on html copy indax

--- purgecss__app.js
+++ purgecss__app.js
@@ -3,11 +3,23 @@ import { writeFileSync } from 'fs';
 import { log } from 'console';
 
 (async () => {
-  const purgecss = await new PurgeCSS().purge('./purgecss.config.js');
+  const purgecss = await new PurgeCSS().purge({
+    content: ['*.html', './assets/maugallery.js', './assets/scripts.js', './assets/bootstrap/bootstrap.bundle.js'],
+    css: ['./assets/bootstrap/boot.min.css' ],
+    safelist: ['mb-4', 'col-12', 'col-sm-6', 'col-md-4', 'col-lg-4', 'col-xl-4'],
+    keyframes: true,
+    fontFace: true,
+    variables: true,
+    rejected: true,
+    rejectedCss: true
 
-  purgecss.forEach((file, index) => {
-    writeFileSync(`./assets/purged/style.min${index}.css`, file.css);
-    writeFileSync(`./assets/purged/rejected${index}.css`, file.rejected);
+});
+
+purgecss.forEach((file, index) => {
+    writeFileSync(`./assets/purged/style.min5.${index}.css`, file.css);
+    writeFileSync(`./assets/purged/rejected.min5.${index}.css`, file.rejectedCss);
   });
     // create file for rejected css
+    //  './assets/bootstrap/bootstrap.bundle.min.js'
 })();
+
